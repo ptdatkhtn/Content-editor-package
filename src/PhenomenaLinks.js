73160de52881de3722719a12915bb464981d957e
Sharif Fadhil <sharif.fadhil@futuresplatform.com>
2020-03-03 10:06:53 +0200

    Changing from class component to functional

--- src/PhenomenaLinks.js
+++ src/PhenomenaLinks.js
@@ -1,62 +1,64 @@
-import React, { Component } from 'react'
-import { requestTranslation } from '@sangre-fp/i18n'
+import React, {useCallback} from 'react'
+import {requestTranslation} from '@sangre-fp/i18n'
 
 
-export default class PhenomenaLinks extends Component {
-    state = { values: [{ value: null }] };
+const PhenomenaLinks = ({values,onChange}) => {
+    const handleChange = useCallback((index, event) => {
+        onChange(values.map((prevValue, i) =>
+            i === index
+                ? {...prevValue, value: event.target.value}
+                : prevValue
+        ))
+    },[values, onChange]);
 
-    handleChange(i, event) {
-        const values = [...this.state.values];
-        values[i].value = event.target.value;
-        this.setState({ values });
-    }
-
-    addMore() {
-        this.setState(prevState => ({
-            values: [...prevState.values, { value: null }]
-        }));
-    }
-
-    deleteClick(i) {
-        const values = [...this.state.values];
-        values.splice(i, 1);
-        this.setState({ values });
-    }
-
-
-    render() {
-        return (
-            <form>
-                {this.state.values.map((linkVal, i) => (
-                    <div key={i}>
-                        <input className="form-control form-control-lg"
-                               type={"text"}
-                               name="linkToWeb"
-                               placeholder="https://"
-                               value={linkVal.value || ""}
-                               onChange={e => this.handleChange(i, e)}
-                        />
-                        <span className="af-custom-close"
-                              style={{
-                                  color: '#006998',
-                                  position: 'relative',
-                                  display: 'inline-block',
-                                  left: '101%',
-                                  bottom: '2.5rem'}}
-                              onClick={() => this.deleteClick(i)}
-                        />
-                    </div>
-                ))}
-
-                <input className="btn btn-plain"
-                       style={{
-                           position: 'relative',
-                           display: 'inline-block'
-                       }}
-                       type="button"
-                       value={requestTranslation ("LinksLabels")}
-                       onClick={() => this.addMore()} />
-            </form>
+    const addMore = useCallback(() => {
+        onChange(
+            [...values, {value: null}]
         );
-    }
-}
+    }, [values, onChange]);
+
+    const deleteClick = useCallback((i) => {
+        const newValues = [...values];
+        newValues.splice(i, 1);
+        onChange(newValues);
+    }, [values, onChange]);
+
+
+    return (
+        <form>
+            {values.map((linkVal, i) => (
+                <div key={i}>
+                    <input className="form-control form-control-lg"
+                           type={"text"}
+                           name="linkToWeb"
+                           placeholder="https://"
+                           value={linkVal.value || ""}
+                           onChange={e => handleChange(i, e)}
+                    />
+                    <span className="af-custom-close"
+                          style={{
+                              color: '#006998',
+                              position: 'relative',
+                              display: 'inline-block',
+                              left: '101%',
+                              bottom: '2.5rem'
+                          }}
+                          onClick={() => deleteClick(i)}
+                    />
+                </div>
+            ))}
+
+            <input className="btn btn-plain"
+                   style={{
+                       position: 'relative',
+                       display: 'inline-block'
+                   }}
+                   type="button"
+                   value={requestTranslation("LinksLabels")}
+                   onClick={addMore}/>
+        </form>
+    );
+
+};
+
+export default PhenomenaLinks;
