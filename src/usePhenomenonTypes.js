ed44d8fee5b219a58d23fa47e7790bd0d02af5ca
Aleksander <aleksander.kalamus@sangre.fi>
2020-05-26 14:01:48 +0200

    FPM-1148 unified order of phenomena types in filters/creator

--- src/usePhenomenonTypes.js
+++ src/usePhenomenonTypes.js
@@ -2,27 +2,6 @@ import { useState, useEffect } from "react"
 import drupalApi from "@sangre-fp/connectors/drupal-api"
 import { keyBy, sortBy } from "lodash-es"
 
-const sortedTypes = types => {
-  if (types.length) {
-    const customTypes = [...types]
-    const defaultTypes = types.slice(Math.max(types.length - 6, 0))
-
-    defaultTypes[1].order = 1
-    defaultTypes[3].order = 4
-    defaultTypes[0].order = 6
-    defaultTypes[4].order = 5
-    defaultTypes[5].order = 3
-    defaultTypes[2].order = 2
-
-    const sortedTypes = sortBy(defaultTypes, 'order')
-    customTypes.splice(customTypes.length - 6)
-
-    return [...customTypes, ...sortedTypes]
-  }
-
-  return types
-}
-
 export const usePhenomenonTypes = groupId => {
   const [phenomenonTypes, setPhenomenonTypes] = useState([])
   const [loading, setLoading] = useState(true)
@@ -46,7 +25,7 @@ export const usePhenomenonTypes = groupId => {
   }, [groupId])
 
   return {
-    phenomenonTypes: sortedTypes(phenomenonTypes),
+    phenomenonTypes: phenomenonTypes,
     phenomenonTypesById: keyBy(phenomenonTypes, "id"),
     loading,
     error
